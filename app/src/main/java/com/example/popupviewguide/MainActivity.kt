0c72e56f3594390a7fb2f7003ce6163063a47955
Johan Mosquera <joncamilomoni@gmail.com>
2021-01-07 11:53:33 -0500

    Refactor Library
    Se crea la biblioteca para usar el tutorial con los popups

--- app/src/main/java/com/example/popupviewguide/MainActivity.kt
+++ app/src/main/java/com/example/popupviewguide/MainActivity.kt
@@ -1,166 +1,149 @@
 package com.example.popupviewguide
 
-import android.annotation.SuppressLint
-import android.graphics.Point
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
 import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
 import com.example.popupviewguide.databinding.ActivityMainBinding
-import com.example.popupviewguide.databinding.FbPopupLayoutBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bindind: ActivityMainBinding
-    private lateinit var popupWindow: PopupWindow
+
+    private var popupView = PopupView()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindind.root)
 
-        bindind.btnTopLeft.setOnClickListener { view ->
-            onShowPopup(view, PositionPopup.BOTTOM_RIGHT)
-        }
-
-        bindind.btnTopRight.setOnClickListener { view ->
-            onShowPopup(view, PositionPopup.BOTTOM_LEFT)
-        }
-
-        bindind.btnBottomLeft.setOnClickListener { view ->
-            onShowPopup(view, PositionPopup.TOP_RIGHT)
-        }
-
-        bindind.btnBottomRight.setOnClickListener { view ->
-            onShowPopup(view, PositionPopup.TOP_LEFT)
-        }
-
-        bindind.btnLeft.setOnClickListener { view ->
-            onShowPopup(view, PositionPopup.RIGHT)
-        }
-
-        bindind.btnRight.setOnClickListener { view ->
-            onShowPopup(view, PositionPopup.LEFT)
-        }
-
-        bindind.btnCenter.setOnClickListener { view ->
-            onShowPopup(view, PositionPopup.TOP_LEFT)
-        }
+        bindind.btnTopLeft.setOnClickListener { view -> clickedTopLeft(view) }
+        bindind.btnTopRight.setOnClickListener { view -> clickedTopRight(view) }
+        bindind.btnRight.setOnClickListener { view -> clickedRight(view) }
+        bindind.btnBottomRight.setOnClickListener { view -> clickedBottomRight(view) }
+        bindind.btnBottomLeft.setOnClickListener { view -> clickedBottomLeft(view) }
+        bindind.btnLeft.setOnClickListener { view -> clickedLeft(view) }
+        bindind.btnCenter.setOnClickListener { view -> clickedCenter(view) }
 
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    private fun onShowPopup(
-        view: View,
-        position: PositionPopup = PositionPopup.DEFAULT,
-        screenRatioSize: FloatArray = floatArrayOf(0.66F, 0.2F)
-    ) {
-
-        //Create a View object yourself through inflater
-        val layoutInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        // inflate the custom popup layout
-        val inflatedView = FbPopupLayoutBinding.inflate(layoutInflater, null, false)
-
-        // Get position from relative to inflate
-        val location = IntArray(2)
-        view.getLocationOnScreen(location)
-        val point = Point(location[0], location[1])
-
-
-        // get device size
-        val display = windowManager.defaultDisplay
-        val size = Point()
-        display.getSize(size)
-
-        var offsetWidth = 50
-        var width = (size.x * screenRatioSize[0]).toInt()
-        var height = (size.y * screenRatioSize[1]).toInt()
-//        var height = LinearLayout.LayoutParams.WRAP_CONTENT
-
-        //Create a window with our parameters
-        popupWindow = PopupWindow(inflatedView.root, width, height, true)
-        popupWindow.isOutsideTouchable = true
-
-        //Handler for clicking on the inactive zone of the window
-        inflatedView.root.setOnTouchListener { v, event -> //Close the window when clicked
-            popupWindow.dismiss()
-            true
-        }
-
-        Log.e("TAG", "onShowPopup: ${popupWindow.height} : ${popupWindow.width}")
-
-        //Set the location of the window on the screen
-        when (position) {
-
-            PositionPopup.BOTTOM_RIGHT -> {
-                popupWindow.showAtLocation(
-                    view, Gravity.NO_GRAVITY,
-                    point.x,
-                    point.y + height / 3
-                )
+    private fun clickedTopLeft(view: View) {
+        popupView.showPopupWindow(view, windowManager, PopupView.PositionPopup.BOTTOM_RIGHT)
+        popupView.setOnClickListener(object : PopupView.OnClickListener {
+            override fun onPrevius(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                Log.e("clickedTopLeft", "onPrevius: ")
             }
 
-            PositionPopup.BOTTOM_LEFT -> {
-                popupWindow.showAtLocation(
-                    view, Gravity.NO_GRAVITY,
-                        point.x - width/2,
-                        point.y + height / 3
-                )
+            override fun onNext(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnTopRight.callOnClick()
+                Log.e("clickedTopLeft", "onNext: ")
             }
-
-            PositionPopup.TOP_RIGHT -> {
-                popupWindow.showAtLocation(
-                    view, Gravity.NO_GRAVITY,
-                    point.x,
-                    point.y - height
-                )
-            }
-
-            PositionPopup.TOP_LEFT -> {
-                popupWindow.showAtLocation(
-                    view, Gravity.NO_GRAVITY,
-                        point.x - width*2/3,
-                    point.y - height
-                )
-            }
-
-            PositionPopup.LEFT -> {
-                popupWindow.showAtLocation(
-                    view, Gravity.NO_GRAVITY,
-                    point.x - (width + offsetWidth),
-                    point.y - height / 3
-                )
-            }
-
-            PositionPopup.RIGHT -> {
-                popupWindow.showAtLocation(
-                    view, Gravity.NO_GRAVITY,
-                    point.x + view.width + offsetWidth,
-                    point.y - height / 3
-                )
-            }
-
-            else -> {
-                //Set the location of the window on the screen
-                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-            }
-        }
-
-
+        })
     }
 
-    enum class PositionPopup {
-        TOP_LEFT,
-        TOP_RIGHT,
-        BOTTOM_LEFT,
-        BOTTOM_RIGHT,
-        LEFT,
-        RIGHT,
-        DEFAULT
+    private fun clickedTopRight(view: View) {
+        popupView.showPopupWindow(view, windowManager, PopupView.PositionPopup.BOTTOM_LEFT)
+        popupView.setOnClickListener(object : PopupView.OnClickListener {
+            override fun onPrevius(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnTopLeft.callOnClick()
+                Log.e("clickedTopRight", "onPrevius: ")
+            }
+
+            override fun onNext(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnRight.callOnClick()
+                Log.e("clickedTopRight", "onNext: ")
+            }
+        })
+    }
+
+    private fun clickedRight(view: View) {
+        popupView.showPopupWindow(view, windowManager, PopupView.PositionPopup.LEFT)
+        popupView.setOnClickListener(object : PopupView.OnClickListener {
+            override fun onPrevius(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnTopRight.callOnClick()
+                Log.e("clickedTopRight", "onPrevius: ")
+            }
+
+            override fun onNext(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnBottomRight.callOnClick()
+                Log.e("clickedTopRight", "onNext: ")
+            }
+        })
+    }
+
+    private fun clickedBottomRight(view: View) {
+        popupView.showPopupWindow(view, windowManager, PopupView.PositionPopup.TOP_LEFT)
+        popupView.setOnClickListener(object : PopupView.OnClickListener {
+            override fun onPrevius(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnRight.callOnClick()
+                Log.e("clickedBottomRight", "onPrevius: ")
+            }
+
+            override fun onNext(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnBottomLeft.callOnClick()
+                Log.e("clickedBottomRight", "onNext: ")
+            }
+        })
+    }
+
+    private fun clickedBottomLeft(view: View) {
+        popupView.showPopupWindow(view, windowManager, PopupView.PositionPopup.TOP_RIGHT)
+        popupView.setOnClickListener(object : PopupView.OnClickListener {
+            override fun onPrevius(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnBottomRight.callOnClick()
+                Log.e("clickedBottomLeft", "onPrevius: ")
+            }
+
+            override fun onNext(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnLeft.callOnClick()
+                Log.e("clickedBottomLeft", "onNext: ")
+            }
+        })
+    }
+
+    private fun clickedLeft(view: View) {
+        popupView.showPopupWindow(view, windowManager, PopupView.PositionPopup.RIGHT)
+        popupView.setOnClickListener(object : PopupView.OnClickListener {
+            override fun onPrevius(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnBottomLeft.callOnClick()
+                Log.e("clickedBottomLeft", "onPrevius: ")
+            }
+
+            override fun onNext(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnCenter.callOnClick()
+                Log.e("clickedBottomLeft", "onNext: ")
+            }
+        })
+    }
+
+    private fun clickedCenter(view: View) {
+        popupView.showPopupWindow(view, windowManager, PopupView.PositionPopup.BOTTOM_LEFT)
+        popupView.setOnClickListener(object : PopupView.OnClickListener {
+            override fun onPrevius(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                bindind.btnLeft.callOnClick()
+                Log.e("clickedBottomLeft", "onPrevius: ")
+            }
+
+            override fun onNext(popupWindow: PopupWindow) {
+                popupWindow.dismiss()
+                Log.e("clickedBottomLeft", "onNext: ")
+            }
+        })
     }
 
 }
--- app/src/main/java/com/example/popupviewguide/PopupView.kt
+++ app/src/main/java/com/example/popupviewguide/PopupView.kt
@@ -1,13 +1,147 @@
 package com.example.popupviewguide
 
+import android.annotation.SuppressLint
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.graphics.Point
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.WindowManager
+import android.widget.PopupWindow
+import com.example.popupviewguide.databinding.FbPopupLayoutBinding
 
 
 class PopupView {
 
-    //PopupWindow display method
-    fun showPopupWindow(view: View) {
+    private lateinit var popupWindow: PopupWindow
+    private lateinit var mOnClickListener: OnClickListener
 
+    interface OnClickListener {
+        fun onPrevius(popupWindow: PopupWindow)
+        fun onNext(popupWindow: PopupWindow)
+    }
+
+    //PopupWindow display method
+    @SuppressLint("ClickableViewAccessibility")
+    fun showPopupWindow(view: View,
+                        windowManager: WindowManager,
+                        position: PositionPopup = PositionPopup.DEFAULT,
+                        screenRatioSize: FloatArray = floatArrayOf(0.66F, 0.2F)) {
+
+        //Create a View object yourself through inflater
+        val layoutInflater = view.context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        // inflate the custom popup layout
+        val inflatedView = FbPopupLayoutBinding.inflate(layoutInflater, null, false)
+
+        // Get position from relative to inflate
+        val location = IntArray(2)
+        view.getLocationOnScreen(location)
+        val point = Point(location[0], location[1])
+
+
+        // get device size
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+
+        var offsetWidth = 50
+        var width = (size.x * screenRatioSize[0]).toInt()
+        var height = (size.y * screenRatioSize[1]).toInt()
+//        var height = LinearLayout.LayoutParams.WRAP_CONTENT
+
+        //Create a window with our parameters
+        popupWindow = PopupWindow(inflatedView.root, width, height, true)
+        popupWindow.isOutsideTouchable = true
+
+        //Handler for clicking on the inactive zone of the window
+        /*inflatedView.root.setOnTouchListener { v, event -> //Close the window when clicked
+            popupWindow.dismiss()
+            true
+        }*/
+
+        Log.e("TAG", "onShowPopup: ${popupWindow.height} : ${popupWindow.width}")
+
+        inflatedView.btnPrevius.setOnClickListener {
+            if (mOnClickListener != null)
+                mOnClickListener.onPrevius(popupWindow)
+        }
+
+        inflatedView.btnNext.setOnClickListener {
+            if (mOnClickListener != null)
+                mOnClickListener.onNext(popupWindow)
+        }
+
+        //Set the location of the window on the screen
+        when (position) {
+
+            PositionPopup.BOTTOM_RIGHT -> {
+                popupWindow.showAtLocation(
+                        view, Gravity.NO_GRAVITY,
+                        point.x,
+                        point.y + height / 3
+                )
+            }
+
+            PositionPopup.BOTTOM_LEFT -> {
+                popupWindow.showAtLocation(
+                        view, Gravity.NO_GRAVITY,
+                        point.x - width / 2,
+                        point.y + height / 3
+                )
+            }
+
+            PositionPopup.TOP_RIGHT -> {
+                popupWindow.showAtLocation(
+                        view, Gravity.NO_GRAVITY,
+                        point.x,
+                        point.y - height
+                )
+            }
+
+            PositionPopup.TOP_LEFT -> {
+                popupWindow.showAtLocation(
+                        view, Gravity.NO_GRAVITY,
+                        point.x - width * 2 / 3,
+                        point.y - height
+                )
+            }
+
+            PositionPopup.LEFT -> {
+                popupWindow.showAtLocation(
+                        view, Gravity.NO_GRAVITY,
+                        point.x - (width + offsetWidth),
+                        point.y - height / 3
+                )
+            }
+
+            PositionPopup.RIGHT -> {
+                popupWindow.showAtLocation(
+                        view, Gravity.NO_GRAVITY,
+                        point.x + view.width + offsetWidth,
+                        point.y - height / 3
+                )
+            }
+
+            else -> {
+                //Set the location of the window on the screen
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+            }
+        }
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        mOnClickListener = onClickListener
+    }
+
+    enum class PositionPopup {
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT,
+        LEFT,
+        RIGHT,
+        DEFAULT
     }
 
 }
